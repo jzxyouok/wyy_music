e2876cec46d2c97cb1a7001a83ae1c46a6a2a360
Mr. Guo <474224024@qq.com>
2016-11-19 20:40:56 +0800

    使用vue版本的UIkit框架

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App'
 import vueRouter from 'vue-router'
 import routerMap from './global/reouter'
 import vueResource from 'vue-resource'
+import Vuikit from 'vuikit'
 
 Vue.use(vueRouter);
 Vue.use(vueResource);
+Vue.use(Vuikit);
 
 //  创建路由实例配置
 const router = new vueRouter({
