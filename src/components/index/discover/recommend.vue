fa5e5b310732c392bc5772edfde133925179bc3b
Mr. Guo <474224024@qq.com>
2016-11-23 14:19:26 +0800

    个性推荐-底部排序按钮

--- src/components/index/discover/recommend.vue
+++ src/components/index/discover/recommend.vue
@@ -16,10 +16,30 @@
         <recommend-mv></recommend-mv>
         <!--  主播电台  -->
         <radio-fm></radio-fm>
+        <!--  底部排序功能  -->
+        <div class="resort-container">
+          <p class="wyy-gray-color uk-text-center resort-tips">现在可以根据个人喜好，自由调整首页栏目顺序啦~</p>
+          <div class="resort uk-text-center">
+            <button class="resort-button uk-button wyy-color">调整栏目顺序</button>
+          </div>
+        </div>
       </div>
     </div>
 </template>
 <style scoped>
+  .resort-container {
+    margin: 10px 0;
+    padding: 10px 0;
+    border-top: 1px solid #f3f3f3;
+  }
+  .resort-tips {
+    font-size: 12px;
+  }
+  .resort-button {
+    background-color: transparent;
+    border: 1px solid #CE3D3E;
+    border-radius: 2px;
+  }
 </style>
 <script>
   import banner from './banner.vue'
